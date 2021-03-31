83402bb330c7ab56f52a5df1e1b8b17055b14e02
Mael <mael.thomas@data.gouv.fr>
2021-03-31 20:37:10 +0200

    Calcul de la gare la plus proche

--- src/views/comparator/Results.js
+++ src/views/comparator/Results.js
@@ -6,6 +6,8 @@ import TransportationContext from "utils/TransportationContext";
 import SearchContext from "utils/SearchContext";
 
 import Transportation from "./results/Transportation";
+import gares from "../../gares.json";
+import { distance, point } from "@turf/turf";
 
 const Wrapper = styled.div`
   flex: 1;
@@ -24,10 +26,27 @@ export default function Results() {
 
   console.log(itinerary);
 
+  const gareProche = gares.sort(
+    (g1, g2) => gareDistance(g1, itinerary) - gareDistance(g2, itinerary)
+  );
+
+  console.log(gareProche);
+
   return (
     <Wrapper>
       Votre trajet fait à vol d'oiseau {km} km.
-      <br />- Super ! Cela me fait une belle jambe !
+      <br />- Super ! Cela me fait une belle jambe !{gareProche[0].libelle}
     </Wrapper>
   );
 }
+
+const gareDistance = (station, itinerary) => {
+  const [lat, long] = station.coordonnées;
+
+  const from = point([
+    Number(itinerary.toLongitude),
+    Number(itinerary.toLatitude),
+  ]);
+  const to = point([long, lat]);
+  return distance(from, to);
+};

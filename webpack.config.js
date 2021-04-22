b4077ddec83a4fd34b4f60d41cad6ac1c24ea273
Mael <mael.thomas@data.gouv.fr>
2021-04-22 17:27:34 +0200

    :fire: Mauvaise tentative de passage de token SNCF

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,9 @@
 const path = require("path");
-const webpack = require("webpack");
 
-console.log(SNCF_TOKEN);
 module.exports = {
   entry: "./iframe/index.js",
   output: {
     filename: "iframe.js",
     path: path.resolve(__dirname, "build"),
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      SNCF_TOKEN: JSON.stringify(process.env.SNCF_TOKEN),
-    }),
-  ],
 };

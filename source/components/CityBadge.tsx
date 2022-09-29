418be78426322bec55a8bb78874e03001cd42a46
Mael <mael.thomas@data.gouv.fr>
2022-09-29 18:24:01 +0200

    Fichiers manquants

---
+++ source/components/CityBadge.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export default styled.small`
+	background: var(--color);
+	border-radius: 0.2rem;
+	color: white;
+	padding: 0.1rem 0.3rem;
+	margin: 0 0.2rem;
+`
---
+++ source/utils.ts
@@ -0,0 +1,2 @@
+export const capitalise0 = (text) =>
+	text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
